58e6c62f9ed498db080d894cdc65e035799baf85
Michael Millar <mcmelord@gmail.com>
2023-09-17 00:02:27 -0600

    add testGetConnection and testAsSqlTimestamp tests

--- src/test/java/homesafe/dao/DAOUtilsTest.java
+++ src/test/java/homesafe/dao/DAOUtilsTest.java
@@ -1,8 +1,36 @@
 package homesafe.dao;
 
+import homesafe.UnitTest;
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.Connection;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 /**
  * created by:
  * author: MichaelMillar
  */
 public class DAOUtilsTest {
+
+    @UnitTest
+    public void testGetConnection() {
+        Connection conn = DAOUtils.getConnection();
+        Assertions.assertNotNull(conn);
+    }
+
+    @UnitTest
+    public void testAsSqlTimestamp() {
+        LocalDateTime dt = LocalDateTime.now();
+        Timestamp ts = new Timestamp(dt.getYear() - 1900,
+                                     dt.getMonthValue() - 1,
+                                     dt.getDayOfMonth(),
+                                     dt.getHour(),
+                                     dt.getMinute(),
+                                     dt.getSecond(),
+                                     dt.getNano());
+
+        Assertions.assertEquals(ts, Timestamp.valueOf(dt));
+    }
+
 }
