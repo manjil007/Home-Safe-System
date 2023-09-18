c5ea47eec132a882b8cd5352f1ed9e194602374e
Michael Millar <mcmelord@gmail.com>
2023-09-18 17:44:07 -0600

    create base event class
    Created the base abstract event class all other events will extend

---
+++ src/main/java/homesafe/entity/AbstractSafeEvent.java
@@ -0,0 +1,19 @@
+package homesafe.entity;
+
+/**
+ * Abstract base class for safe events. Each "event publisher" will have their
+ * own event class.
+ */
+public abstract class AbstractSafeEvent {
+
+    private final String eventType;
+
+    protected AbstractSafeEvent(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+}
