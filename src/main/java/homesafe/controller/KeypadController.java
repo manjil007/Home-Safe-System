cdeb9a8e1ad3c17acedff0c38c04e8aceebf40dc
Michael Millar <mcmelord@gmail.com>
2023-09-24 20:41:57 -0600

    created and started implementation of KeypadController

---
+++ src/main/java/homesafe/controller/KeypadController.java
@@ -0,0 +1,16 @@
+package homesafe.controller;
+
+/**
+ * created by:
+ * author: MichaelMillar
+ */
+public class KeypadController extends AbstractController {
+
+    public KeypadController() {
+    }
+
+    @Override
+    public void run() {
+        
+    }
+}
