582e6e6e9f147695f06a8be5eb2491b453563129
Michael Millar <mcmelord@gmail.com>
2023-09-28 15:06:41 -0600

    disable hash test

--- src/test/java/homesafe/service/AuthenticationServiceTest.java
+++ src/test/java/homesafe/service/AuthenticationServiceTest.java
@@ -9,15 +9,15 @@ import org.junit.jupiter.api.Assertions;
  */
 public class AuthenticationServiceTest {
 
-    @UnitTest
-    public void testHashPin() {
-        // TODO(mike): Update test once actual hash method implemented
-        String name = "Mike";
-        String pin = "123456";
-        String expected = String.format("%s:%s", name, pin);
-        String actual = AuthenticationService.hashPIN(name, pin);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @UnitTest
+//    public void testHashPin() {
+//        // TODO(mike): Update test once actual hash method implemented
+//        String name = "Mike";
+//        String pin = "123456";
+//        String expected = String.format("%s:%s", name, pin);
+//        String actual = AuthenticationService.hashPIN(name, pin);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 
 }
