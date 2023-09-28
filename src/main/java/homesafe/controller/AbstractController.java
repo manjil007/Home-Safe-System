ee33903b4bff0d1f7fd3049b9c9dfccfd281397c
Michael Millar <mcmelord@gmail.com>
2023-09-27 19:26:43 -0600

    add a run boolean, and stop method
    Added a boolean 'run' and a method 'stop' so that we can stop the sensors/controllers if needed

--- src/main/java/homesafe/controller/AbstractController.java
+++ src/main/java/homesafe/controller/AbstractController.java
@@ -10,9 +10,15 @@ import homesafe.service.EventService;
  */
 public abstract class AbstractController implements Runnable, SafeEventPublisher<AbstractSafeEvent> {
 
+    protected boolean run = true;
+
     @Override
     public void publishEvent(AbstractSafeEvent event) {
         EventService.getInstance().publishEvent(event);
+    }
+
+    public void stop() {
+        this.run = false;
     }
 
 }
