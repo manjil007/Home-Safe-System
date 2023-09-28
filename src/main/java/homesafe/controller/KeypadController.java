d2e86ce2ba6034926ff98ca6e9f1cc96ad2237d7
Michael Millar <mcmelord@gmail.com>
2023-09-27 19:30:59 -0600

    Implemented logic for the Keypad controller

--- src/main/java/homesafe/controller/KeypadController.java
+++ src/main/java/homesafe/controller/KeypadController.java
@@ -1,16 +1,75 @@
 package homesafe.controller;
 
+import homesafe.entity.ApplicationState;
+import homesafe.entity.DataObject;
+import homesafe.entity.EditAdminDataObject;
+import homesafe.entity.EditSelfDataObject;
+import homesafe.entity.LoginDataObject;
+import homesafe.entity.State;
+import homesafe.event.ButtonEvent;
+import homesafe.event.SafeEventHandler;
+import homesafe.service.AuthenticationService;
+
 /**
  * created by:
  * author: MichaelMillar
  */
-public class KeypadController extends AbstractController {
+public class KeypadController extends AbstractController implements SafeEventHandler<ButtonEvent> {
 
-    public KeypadController() {
+    private static KeypadController instance;
+
+    private DataObject currentDataObject;
+    private State currentState;
+
+    public static KeypadController getInstance() {
+        if (instance == null) {
+            instance = new KeypadController();
+        }
+        return instance;
+    }
+
+    private KeypadController() {    }
+
+    private void monitorState() {
+        State state = ApplicationState.getInstance().getState();
+        if (state != currentState) {
+            currentState = state;
+            switch (state) {
+                case IDLE, VIEW_LOGS, OPEN, LOCKOUT -> currentDataObject = null;
+                case LOGIN -> currentDataObject = new LoginDataObject();
+                case ADMIN_EDIT_USER -> currentDataObject =
+                        new EditAdminDataObject(AuthenticationService.getCurrentUser());
+                case SELF_EDIT_USER -> currentDataObject =
+                        new EditSelfDataObject(AuthenticationService.getCurrentUser());
+            }
+        }
     }
 
     @Override
     public void run() {
-        
+        while (run) {
+            monitorState();
+        }
+    }
+
+    @Override
+    public void handleEvent(ButtonEvent event) {
+        switch (event.getEventType()) {
+            case ButtonEvent.FIELD_FOCUSED -> {
+                if (currentDataObject != null) {
+                    currentDataObject.setActiveName(event.getData());
+                }
+            }
+            case ButtonEvent.KEY_PRESSED -> {
+                if (currentDataObject != null) {
+                    currentDataObject.appendToActiveData(event.getData());
+                }
+            }
+            case ButtonEvent.PROCESS_FORM -> {
+                if (currentDataObject != null) {
+                    currentDataObject.process();
+                }
+            }
+        }
     }
 }
